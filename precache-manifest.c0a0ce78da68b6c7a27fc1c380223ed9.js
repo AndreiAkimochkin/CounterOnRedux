fdea6dc5e049dd92697d2ecb0cebee05561432b6
AndreiAkimochkin <AndreiSverige@gmail.com>
2021-03-07 17:14:18 +0100

    Updates

---
+++ precache-manifest.c0a0ce78da68b6c7a27fc1c380223ed9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5a66a4cb83952e999e03d4539e7322",
+    "url": "/CounterOnRedux/index.html"
+  },
+  {
+    "revision": "9d929dd0d02dfb76adb9",
+    "url": "/CounterOnRedux/static/css/main.adf6d95e.chunk.css"
+  },
+  {
+    "revision": "00c9744f16879036c95a",
+    "url": "/CounterOnRedux/static/js/2.f2adca9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CounterOnRedux/static/js/2.f2adca9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d929dd0d02dfb76adb9",
+    "url": "/CounterOnRedux/static/js/main.828f8b79.chunk.js"
+  },
+  {
+    "revision": "5e30058086d0576ad436",
+    "url": "/CounterOnRedux/static/js/runtime-main.fbb5cfbc.js"
+  }
+]);
